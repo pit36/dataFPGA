294a8c0b53ebf6e55d20da0244ab760f7ef9f9f7
pit.36 <p.sossalla@googlemail.com>
2019-01-07 17:21:44 +0100

    changed startup script

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,5 +1,31 @@
-bash start_ovs.sh
-bash config_network_interfaces.sh
+#bash $2/start_ovs.sh
+#bash $2/config_network_interfaces.sh
+
+# start OVS
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+                     --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+                     --private-key=db:Open_vSwitch,SSL,private_key \
+                     --certificate=db:Open_vSwitch,SSL,certificate \
+                     --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+                     --pidfile --detach
+ovs-vsctl --no-wait init
+ovs-vswitchd --pidfile --detach
+
+
+# config NW interfaces
+modprobe sume_riffa
+# set interfaces up, normally are up but to be sure lol
+ip link set dev nf0 up
+ip link set dev nf1 up
+ip link set dev nf2 up
+ip link set dev nf3 up
+# add to bonds
+ovs-vsctl add-br br0
+ovs-vsctl add-br br1
+ovs-vsctl add-bond br0 bond0 nf0 nf1
+ovs-vsctl add-bond br1 bond1 nf2 nf3
+
+
 # add ip to the bridge
 # bigOne: 10.0.1.1
 # vw1: 10.0.1.2
