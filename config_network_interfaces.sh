bdfba5bb072ae2c6288a9ad86da6c533beb17a24
pit36 <you@example.com>
2019-01-07 12:28:19 +0100

    NW interfaces config added

---
+++ config_network_interfaces.sh
@@ -0,0 +1,6 @@
+modprobe sume_riffa
+# set interfaces up, normally are up but to be sure lol
+sudo ip link set dev nf0 up
+sudo ip link set dev nf1 up
+sudo ip link set dev nf2 up
+sudo ip link set dev nf3 up
